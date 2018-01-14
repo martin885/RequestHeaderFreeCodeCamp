17478528a0107f4edad32c25b347a7257d10bd1e
martin885 <mat885@hotmail.com>
2018-01-14 14:41:47 -0300

    sexto commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict'
-const express=require('express');
- const moduleHeader=require('./moduleHeader');
- var port = Number(process.env.PORT||8080);
+const express = require('express');
+const moduleHeader = require('./moduleHeader');
+var port = Number(process.env.PORT || 8080);
 
-const app =express();
- app.use('/',express.static(__dirname+'/public'));
-app.get('/api/whoami/',function(req,res){
-    
-    var ip= req.connection.remoteAddress|| req.ip || 
-    req.socket.remoteAddress ;
-console.log(req.socket.remoteAddress,req.ip,req.connection.remoteAddress,req.headers['x-forwarded-for']);
-  res.json(moduleHeader(ip,req.headers["accept-language"],req.headers["user-agent"]));
+const app = express();
+app.use('/', express.static(__dirname + '/public'));
+app.get('/api/whoami/', function (req, res) {
+
+
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    ip = ip.split(',')[0];
+    res.json(moduleHeader(ip, req.headers["accept-language"], req.headers["user-agent"]));
 });
 
 
-app.listen(port,function(){
+app.listen(port, function () {
     console.log('OK');
 });
