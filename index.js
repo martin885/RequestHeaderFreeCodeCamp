ebb3c79e1d2224c386b8cb094ed4e89f4de41936
martin885 <mat885@hotmail.com>
2018-01-14 14:49:50 -0300

    septimo commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use('/', express.static(__dirname + '/public'));
 app.get('/api/whoami/', function (req, res) {
 
 
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress||req.ip;
     ip = ip.split(',')[0];
     res.json(moduleHeader(ip, req.headers["accept-language"], req.headers["user-agent"]));
 });
